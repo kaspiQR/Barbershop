1ee3ae65e8ab121f3e2bcdad23b1596e7ca84b5f
kiri3914 <t.bektur.2002@gmail.com>
2023-04-14 10:16:38 +0600

    templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import (
+    Position,
+    Barber,
+    Testimonial,
+    Contact,
+    Service,
+    Subscribe,
+    Appointment)
 
-# Register your models here.
+
+admin.site.register(Position)
+admin.site.register(Barber)
+admin.site.register(Testimonial)
+admin.site.register(Contact)
+admin.site.register(Service)
+admin.site.register(Subscribe)
+admin.site.register(Appointment)
