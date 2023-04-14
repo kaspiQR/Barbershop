30243fb2b6473e38cb5831c55a1e34b9a8db6a0e
kiri3914 <t.bektur.2002@gmail.com>
2023-04-14 09:54:25 +0600

    templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('appointment', views.appointment, name='appointment'),
+    path('barbers', views.barbers, name='barbers'),
+    path('contact', views.contact, name='contact'),
+    path('services', views.services, name='services'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,23 @@ from .models import (
 
 def index(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
+
+def barbers(request):
+    return render(request, 'barbers.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def services(request):
+    return render(request, 'services.html')
